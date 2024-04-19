d0f43c02ffe22e82da6e1b830ded457d6bd5176c
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-04-19 16:51:23 +0200

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        "primary-1": "#372B50",
+        "secondary-1": "#261132",
+      }
     },
   },
   plugins: [],
