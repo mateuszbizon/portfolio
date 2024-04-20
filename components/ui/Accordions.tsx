7129b205ff9e6f98c3180912d71bf5291ea99021
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-04-20 16:35:02 +0200

    added hero icons

--- components/ui/Accordions.tsx
+++ components/ui/Accordions.tsx
@@ -2,6 +2,7 @@
 
 import useAccordions from "@/hooks/useAccordions";
 import React from "react";
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
 
 type Props = {
     accordions: AccordionType[];
@@ -18,9 +19,10 @@ function Accordions({ accordions }: Props) {
 				return (
 					<div key={item.title} className='border-b border-b-light-3 first:border-t first:border-t-light-3 text-lg md:text-xl'>
 						<button
-							className={`flex justify-between py-4 px-6 hover:bg-light-2 transition duration-300 w-full ${isActive && "bg-light-2 text-primary-2 font-medium"}`}
+							className={`flex justify-between py-5 px-6 hover:bg-light-2 transition duration-300 w-full ${isActive && "bg-light-2 text-primary-2 font-medium"}`}
 							onClick={() => handleSetIndexAccordion(index)}>
 							<span>{item.title}</span>
+                            <ChevronDownIcon className={`h-6 w-6 transition-all duration-300  ${isActive && "rotate-[-180deg]"}`} />
 						</button>
 						<div className={`grid px-6 transition-all duration-200 ${isActive ? "grid-rows-[1fr] py-6" : "grid-rows-[0fr]"}`}>
                             <div className='overflow-hidden' dangerouslySetInnerHTML={{ __html: item.text }}></div>
