4e25892d1f5875a31ee2b5156677c38c6f904361
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-04-20 16:24:44 +0200

    changed titles font size

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,7 @@ import { ABOUT_ACCORDIONS } from "@/constants";
 function About() {
   return (
     <section id='about' className='section main-padding-x'>
-        <h2 className='title-2 mb-14'>O mnie</h2>
+        <h2 className='title-2 title-margin-bottom'>O mnie</h2>
         <Accordions accordions={ABOUT_ACCORDIONS} />
     </section>
   )
