fbf387ba299a26c3d5302733b28efc48e51951ca
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-04-26 14:37:48 +0200

    added learning element with animation

--- components/RoadMap.tsx
+++ components/RoadMap.tsx
@@ -1,8 +1,13 @@
+"use client"
+
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const ROAD_MAP_ITEMS = ["HTML", "CSS", "JavaScript", "Git & Github", "SCSS", "Node.js", "React.js", "Express.js", "TypeScript", "TailwindCSS", "Next.js", "Docker"]
 
 function RoadMap() {
+    const learningElement: string = "Next.js"  
+
   return (
     <section id='road-map' className='section main-padding-x'>
         <h2 className='title-2 title-margin-bottom'>Plan nauki</h2>
@@ -11,10 +16,19 @@ function RoadMap() {
                 const isElementEven = (index + 1) % 2 == 0 ? true : false;
 
                 return (
-                    <div key={item} className={`flex gap-2 sm:gap-8 sm:text-lg absolute ${isElementEven ? "-right-2" : "-left-2"}`} style={{ top: `${index * 100}px` }}>
-                        <div className={`w-5 h-5 bg-primary-1 rounded-full ${isElementEven && "order-1"}`}></div>
-                        <div className='px-4 py-2 w-min text-center bg-light-1 border border-light-3 rounded-lg'>
-                            {item}
+                    <div key={item} className={`absolute ${isElementEven ? "-right-2" : "-left-2"}`} style={{ top: `${index * 100}px` }}>
+                        <div className='relative flex gap-2 sm:gap-8 '>
+                            <div className={`w-5 h-5 bg-primary-1 rounded-full ${isElementEven && "order-1"}`}></div>
+
+                            <div className='px-4 py-2 w-min text-center sm:text-lg bg-light-1 border border-light-3 rounded-lg'>
+                                {item}
+                            </div>
+
+                            {item === learningElement && (
+                                <motion.div className={`absolute top-0 ${isElementEven ? "-right-24 sm:-right-28" : "-left-24 sm:-left-28"} px-4 py-2 w-min text-center text-primary-2 font-medium bg-light-1 border border-primary-1 rounded-lg`} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 3, repeat: Infinity }}>
+                                    Jestem tutaj!
+                                </motion.div>
+                            )}
                         </div>
                     </div>
                 )
