2de79cfcfb18577de846ecd013313512039f76a6
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-04-28 18:50:11 +0200

    moved back to start btn to up

--- components/BackToStart.tsx
+++ components/BackToStart.tsx
@@ -9,7 +9,7 @@ function BackToStart() {
     const { elementActive } = useScroll(500);
 
   return (
-    <Link href="#header" className={`fixed bottom-4 right-4 bg-primary-1 hover:bg-primary-2 rounded-lg p-4 ${elementActive ? "opacity-100" : "opacity-0 pointer-events-none"} transition-all duration-300`}>
+    <Link href="#header" className={`fixed bottom-12 right-4 bg-primary-1 hover:bg-primary-2 rounded-lg p-4 ${elementActive ? "opacity-100" : "opacity-0 pointer-events-none"} transition-all duration-300`}>
         <ChevronDoubleUpIcon className='h-7 w-7 text-light-1' />
     </Link>
   )
