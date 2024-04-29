8debdcc757cb8c38a053f6e3d299bb877c07fcd3
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-04-29 14:15:43 +0200

    moved html projects to component

--- components/Projects/HtmlProjects.tsx
+++ components/Projects/HtmlProjects.tsx
@@ -1,12 +1,12 @@
-import { HTML_CSS_PROJECTS } from '@/constants'
-import React from 'react'
-import ProjectCard from '../cards/ProjectCard'
-import ProjectsContainer from './ProjectsContainer'
+import React from "react";
+import ProjectsContainer from "./ProjectsContainer";
+
+const HTML_CSS_PROJECTS: ProjectType[] = [
+  { title: "Recipe page", img: "/recipe.jpg", alt: "recipe app", link: "https://mateuszbizon.github.io/recipe_page/", github: "https://github.com/mateuszbizon/recipe_page" },
+]
 
 function HtmlProjects() {
-  return (
-    <ProjectsContainer projects={HTML_CSS_PROJECTS} />
-  )
+	return <ProjectsContainer projects={HTML_CSS_PROJECTS} />;
 }
 
-export default HtmlProjects
+export default HtmlProjects;
