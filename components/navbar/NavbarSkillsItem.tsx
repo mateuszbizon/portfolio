b7ed91ebed826cf43d54939e014cfb548ef449a2
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-05-10 14:37:51 +0200

    changed showing skills with button click

--- components/navbar/NavbarSkillsItem.tsx
+++ components/navbar/NavbarSkillsItem.tsx
@@ -1,18 +1,56 @@
 "use client"
 
 import ChevronDownIcon from '@/public/icons/ChevronDownIcon'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+
+const SKILLS_LIST = [
+  { name: "Html" },
+  { name: "Css" },
+  { name: "JavaScript" },
+  { name: "React" },
+  { name: "TailwindCSS" },
+  { name: "Scss" },
+  { name: "Git" },
+  { name: "TypeScript" },
+  { name: "NextJS" },
+]
 
 function NavbarSkillsItem() {
+  const [itemActive, setItemActive] = useState(false)
+  const buttonRef = useRef<HTMLButtonElement>(null)
+
+  useEffect(() => {
+    function closeSkillsItemOutside(event: MouseEvent) {
+      if (buttonRef.current && !buttonRef.current.contains(event.target as Node)) {
+        setItemActive(false)
+      }
+    }
+
+    document.addEventListener("click", closeSkillsItemOutside)
+    
+    return () => {
+      document.removeEventListener("click", closeSkillsItemOutside)
+    }
+  }, [])
+
   return (
-    <div className='relative group'>
-        <div>
-            <span className='navbar-item flex gap-2 items-center cursor-pointer'>Umiejętności <ChevronDownIcon /></span>
+    <button ref={buttonRef} className={`relative navbar-item-pointer-up ${itemActive ? "navbar-item-pointer-up-show" : "navbar-item-pointer-up-hide"}`} onClick={() => setItemActive(prev => !prev)}>
+        <div className={`navbar-item flex gap-2 items-center cursor-pointer ${itemActive && "text-primary-2"}`}>
+            <span className='flex gap-2 items-center'>Umiejętności</span>
+            <div>
+              <ChevronDownIcon />
+            </div>
         </div>
-        <div className='absolute top-[150%] p-3 bg-light-1 left-0 w-[300px] opacity-0 group-hover:opacity-100 transition-all duration-300 z-10 pointer-events-none'>
-            <span className='text-center'>text</span>
+        <div className={`absolute top-[150%] left-0 md:right-0 md:left-auto p-5 bg-light-1 w-[300px] ${itemActive ? "opacity-100" : "opacity-0"} transition-all duration-300 z-10 pointer-events-none rounded-lg`}>
+            <ul className='flex gap-5 flex-wrap text-black'>
+              {SKILLS_LIST.map(item => {
+                return (
+                  <li key={item.name}>{item.name}</li>
+                )
+              })}
+            </ul>
         </div>
-    </div>
+    </button>
   )
 }
 
