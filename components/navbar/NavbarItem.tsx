0a5be90053ea2751334d06767af0bffefa0bef74
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-05-13 14:17:04 +0200

    added animation on navbar item dropdown

--- components/navbar/NavbarItem.tsx
+++ components/navbar/NavbarItem.tsx
@@ -39,18 +39,17 @@ function NavbarItem({ item, setDropdownActive }: Props) {
                 {item.title}
             </Link>
         ) : (
-            <div className={`relative ${itemActive && "navbar-item-pointer"}`} data-dropdown>
+            <div className={`relative navbar-item-pointer ${itemActive ? "after:opacity-100" : "after:opacity-0"}`} data-dropdown>
                 <button className={`navbar-item flex gap-2 items-center cursor-pointer ${itemActive && "text-primary-2"}`} onClick={() => setItemActive(prev => !prev)} data-dropdown-button>
                     <span className='flex gap-2 items-center'>Umiejętności</span>
                     <div className={`${itemActive ? "rotate-180" : "rotate-0"} transition-all`}>
                       <ChevronDownIcon />
                     </div>
                 </button>
-                {itemActive && (
-                    <div className={`absolute top-[150%] left-0 md:right-0 md:left-auto p-5 bg-light-1 w-[300px] sm:w-[500px] transition-all duration-300 z-10 rounded-lg`}>
-                        {item.content}
-                    </div>
-                )}
+
+                <div className={`absolute top-[150%] left-0 md:right-0 md:left-auto p-5 bg-light-1 w-[300px] sm:w-[500px] transition-all z-10 rounded-lg ${itemActive ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}>
+                    {item.content}
+                </div>
             </div>
         )}
     </>
