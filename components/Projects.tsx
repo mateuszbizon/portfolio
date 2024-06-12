0f89dde92e632eda3572dbaef31805207ad58004
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-06-12 14:48:49 +0200

    changed projects structure

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,40 +1,16 @@
 "use client"
 
-import React, { useState } from 'react'
-import HtmlProjects from './Projects/HtmlProjects'
-import JavaScriptProjects from './Projects/JavaScriptProjects'
-import ReactProjects from './Projects/ReactProjects'
-import NextJsProjects from './Projects/NextJsProjects'
+import React from 'react'
 import Container from './layouts/Container'
-
-const PROJECTS = [<HtmlProjects />, <JavaScriptProjects />, <ReactProjects />, <NextJsProjects />]
-
-const PROJECTS_BTNS = ["HTML & CSS", "JavaScript", "React", "Next.js"]
+import ProjectsList from './lists/ProjectsList'
 
 function Projects() {
-    const [btnIndex, setBtnIndex] = useState(0)
-
   return (
     <section id='projects' className='main-padding-x section bg-light-3'>
         <Container>
             <h2 className='title-2 title-margin-bottom'>Projects</h2>
-            <div className='flex gap-5 overflow-auto mb-10'>
-                {PROJECTS_BTNS.map((btnText, index) => {
-                    const isActive = btnIndex == index ? true : false;
-
-                    return (
-                        <button key={btnText} className={`shrink-0 py-4 px-8 rounded-full transition md:text-xl ${!isActive ? "hover:text-primary-2" : "bg-light-1"}`} onClick={() => setBtnIndex(index)}>{btnText}</button>
-                    )
-                })}
-            </div>
             <div className='w-full flex overflow-hidden'>
-                {PROJECTS.map((project, index) => {
-                    return (
-                        <div key={index} className={`w-full shrink-0 transition-all duration-300`} style={{ translate: `${-100 * btnIndex}%` }}>
-                            {project}
-                        </div>
-                    )
-                })}
+                <ProjectsList />
             </div>
         </Container>
     </section>
---
+++ components/lists/ProjectsList.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ProjectCard from '../cards/ProjectCard'
+
+const PROJECTS_LIST: ProjectType[] = [
+    { title: "Piggy bank online", img: "/piggy_bank_online.jpg", alt: "piggy bank online app", link: "https://piggybankonline.vercel.app/", github: "https://github.com/mateuszbizon/piggy_bank_app_nextjs" },
+    { title: "Comments section", img: "/comments_section.jpg", alt: "comments section app", link: "https://comments-section-react.netlify.app/", github: "https://github.com/mateuszbizon/comments_section_react" },
+    { title: "Multi-step form", img: "/multi_step_form.jpg", alt: "multi-step form app", link: "https://multi-stepformreact.netlify.app/", github: "https://github.com/mateuszbizon/multi-step-form-react" },
+    { title: "Social media dashboard", img: "/social_media_dashboard.jpg", alt: "social media dashboard app", link: "https://social-media-dashboard-nextjs-two.vercel.app/", github: "https://github.com/mateuszbizon/social_media_dashboard_nextjs" },
+]
+
+function ProjectsList() {
+  return (
+    <div className='projects-container'>
+        {PROJECTS_LIST.map((item) => {
+            return (
+                <ProjectCard key={item.title} project={item} />
+            )
+        })}
+    </div>
+  )
+}
+
+export default ProjectsList
