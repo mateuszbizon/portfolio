6180c7a588de6a3538c83c4a89e211215f9d27f7
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-06-12 15:06:57 +0200

    changed back to start button background color

--- components/BackToStart.tsx
+++ components/BackToStart.tsx
@@ -9,7 +9,7 @@ function BackToStart() {
     const { elementActive } = useScroll(500);
 
   return (
-    <Link href="#header" className={`fixed bottom-12 right-4 bg-primary-1 hover:bg-primary-2 rounded-lg p-4 ${elementActive ? "opacity-100" : "opacity-0 pointer-events-none"} transition-all duration-300`}>
+    <Link href="#header" className={`fixed bottom-12 right-4 bg-primary-2 hover:bg-primary-1 border-2 border-primary-2 rounded-lg p-4 ${elementActive ? "opacity-100" : "opacity-0 pointer-events-none"} transition-all duration-300`}>
         <div className='text-light-1'>
           <ChevronUpIcon />
         </div>
