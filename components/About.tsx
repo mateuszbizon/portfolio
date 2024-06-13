e633e2400b7a6e50b6738ffe7adedd07c25c7e5f
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-06-13 13:40:22 +0200

    added accordion type

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Accordions from './lists/Accordions'
 import Container from './layouts/Container'
+import { AccordionType } from '@/types'
 
 export const ABOUT_ACCORDIONS: AccordionType[] = [
   { title: "Private", text: "I was born in Lubań (Poland) and I live in Jerzmanki (2 km from Zgorzelec) in the Lower Silesian Voivodeship in Poland. In my free time I solve Rubik's Cube on time. I have been doing this for over 5 years. I took part in several competitions and achieved results around 18 seconds. <br /><br /> In addition I love listening to music and play computer games and course I love creating web applications for varoius purposes." },
