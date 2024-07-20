7f3aa9d745b2cb50dc059104a4eedaded2f97ea7
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-07-20 15:17:26 +0200

    deleted useless technologies from projects list

--- constants/index.ts
+++ constants/index.ts
@@ -18,13 +18,13 @@ export const EXPERIENCE_LIST = [
 ]
 
 export const PROJECTS_LIST: ProjectType[] = [
-    { title: "Product feedback app", img: "/product_feedback.jpg", alt: "product feedback app", link: "https://productfeedbackappfm.netlify.app/", github: "https://github.com/mateuszbizon/product_feedback", technologies: ["Html", "Css", "TypeScript", "Next.js", "TailwindCSS", "MongoDB", "Clerk", "server actions"], isFullstack: true, customIdea: { title: "Frontend Mentor", link: "https://www.frontendmentor.io/challenges" }, },
+    { title: "Product feedback app", img: "/product_feedback.jpg", alt: "product feedback app", link: "https://productfeedbackappfm.netlify.app/", github: "https://github.com/mateuszbizon/product_feedback", technologies: ["TypeScript", "Next.js", "TailwindCSS", "MongoDB", "Clerk", "server actions", "react-hook-form", "zod"], isFullstack: true, customIdea: { title: "Frontend Mentor", link: "https://www.frontendmentor.io/challenges" }, },
 
-    { title: "Piggy bank online", img: "/piggy_bank_online.jpg", alt: "piggy bank online app", link: "https://piggybankonline.vercel.app/", github: "https://github.com/mateuszbizon/piggy_bank_app_nextjs", technologies: ["Html", "Css", "TypeScript", "Next.js", "TailwindCSS", "MongoDB", "Clerk", "server actions"], isFullstack: true, },
+    { title: "Piggy bank online", img: "/piggy_bank_online.jpg", alt: "piggy bank online app", link: "https://piggybankonline.vercel.app/", github: "https://github.com/mateuszbizon/piggy_bank_app_nextjs", technologies: ["TypeScript", "Next.js", "TailwindCSS", "MongoDB", "Clerk", "server actions", "react-hook-form", "zod"], isFullstack: true, },
 
-    { title: "Multi-step form", img: "/multi_step_form.jpg", alt: "multi-step form app", link: "https://multi-stepformreact.netlify.app/", github: "https://github.com/mateuszbizon/multi-step-form-react", technologies: ["Html", "Css", "Scss", "TypeScript", "React"], isFullstack: false, customIdea: { title: "Frontend Mentor", link: "https://www.frontendmentor.io/challenges" }, },
+    { title: "Multi-step form", img: "/multi_step_form.jpg", alt: "multi-step form app", link: "https://multi-stepformreact.netlify.app/", github: "https://github.com/mateuszbizon/multi-step-form-react", technologies: ["Scss", "TypeScript", "React", "react-hook-form", "zod"], isFullstack: false, customIdea: { title: "Frontend Mentor", link: "https://www.frontendmentor.io/challenges" }, },
 
-    { title: "Social media dashboard", img: "/social_media_dashboard.jpg", alt: "social media dashboard app", link: "https://social-media-dashboard-nextjs-two.vercel.app/", github: "https://github.com/mateuszbizon/social_media_dashboard_nextjs", technologies: ["Html", "Css", "TypeScript", "Next.js", "Tailwind"], isFullstack: false, customIdea: { title: "Frontend Mentor", link: "https://www.frontendmentor.io/challenges" }, },
+    { title: "Social media dashboard", img: "/social_media_dashboard.jpg", alt: "social media dashboard app", link: "https://social-media-dashboard-nextjs-two.vercel.app/", github: "https://github.com/mateuszbizon/social_media_dashboard_nextjs", technologies: ["TypeScript", "Next.js", "TailwindCSS"], isFullstack: false, customIdea: { title: "Frontend Mentor", link: "https://www.frontendmentor.io/challenges" }, },
 ]
 
 export const SKILLS_LIST = [
