474b77da5364523fdbdbcd4e76908035dcf761ad
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-07-20 14:37:46 +0200

    header decorative elements animations

--- components/HeaderElements.tsx
+++ components/HeaderElements.tsx
@@ -1,11 +1,14 @@
+"use client"
+
 import React from 'react'
+import { motion } from 'framer-motion'
 
 function HeaderElements() {
   return (
     <>
-        <div className='absolute -top-[150px] right-0 size-[200px] sm:size-[250px] md:size-[300px] lg:size-[400px] bg-gradient-to-tr from-primary-1/10 to-primary-2/20 rounded-full'></div>
+        <motion.div className='absolute -top-[150px] right-0 size-[200px] sm:size-[250px] md:size-[300px] lg:size-[400px] bg-gradient-to-tr from-primary-1/10 to-primary-2/20 rounded-full' animate={{ y: ["0%", "-20%", "0%"] }} transition={{ repeat: Infinity, duration: 10 }}></motion.div>
 
-        <div className='absolute bottom-0 -left-[130px] size-[200px] sm:size-[230px] md:size-[260px] lg:size-[300px] bg-gradient-to-bl from-primary-1/10 to-primary-2/20 rounded-full'></div>
+        <motion.div className='absolute bottom-0 -left-[130px] size-[200px] sm:size-[230px] md:size-[260px] lg:size-[300px] bg-gradient-to-bl from-primary-1/10 to-primary-2/20 rounded-full' animate={{ y: ["0%", "-40%", "0%"] }} transition={{ repeat: Infinity, duration: 10, delay: 5 }}></motion.div>
     </>
   )
 }
