6cb769779324907a59392a5e4da59f4aa8347849
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-02-10 22:20:37 +0100

    added skill level component

--- components/cards/SkillCard.tsx
+++ components/cards/SkillCard.tsx
@@ -1,5 +1,6 @@
 import { Skill } from '@/types'
 import React from 'react'
+import SkillLevel from '../SkillLevel'
 
 type SkillCardProps = {
     skill: Skill;
@@ -7,14 +8,18 @@ type SkillCardProps = {
 
 function SkillCard({ skill }: SkillCardProps) {
   return (
-    <div className='container-2 flex flex-col items-center gap-3'>
-        {skill.icon && (
-            <div className='size-10'>
-                {skill.icon}
-            </div>
+    <div className='container-2 flex flex-col items-center gap-5'>
+        <div className='space-y-3'>
+            {skill.icon && (
+                <div className='size-10'>
+                    {skill.icon}
+                </div>
+            )}
+            <p className='text-center font-bold text-primary-1 text-lg'>{skill.name}</p>
+        </div>
+        {skill.level && (
+            <SkillLevel skillLevel={skill.level} />
         )}
-        <span className='text-center font-bold text-primary-1 text-lg'>{skill.name}</span>
-        <span className='text-center'>{skill.level}</span>
     </div>
   )
 }
