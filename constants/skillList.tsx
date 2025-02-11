1e380315a79d265b8cf8253b787f07e1eda24440
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-02-11 13:52:06 +0100

    added more skills

--- constants/skillList.tsx
+++ constants/skillList.tsx
@@ -5,12 +5,92 @@ export const SKILL_LIST: Group<Skill>[] = [
         name: "Programming Languages",
         items: [
             {
-                name: "Html",
+                name: "HTML",
                 level: "solid",
             },
             {
-                name: "Css",
+                name: "CSS",
                 level: "solid",
+            },
+            {
+                name: "SCSS",
+                level: "solid",
+            },
+            {
+                name: "Typescript",
+                level: "solid",
+            },
+        ]
+    },
+    {
+        name: "Frameworks",
+        items: [
+            {
+                name: "React",
+                level: "solid",
+            },
+            {
+                name: "Next.js",
+                level: "solid",
+            },
+            {
+                name: "TailwindCSS",
+                level: "solid",
+            },
+            {
+                name: "Express.js",
+                level: "basic",
+            },
+        ]
+    },
+    {
+        name: "Databases",
+        items: [
+            {
+                name: "PostgreSQL",
+                level: "basic",
+            },
+            {
+                name: "MongoDB",
+                level: "basic",
+            },
+        ]
+    },
+    {
+        name: "Libraries",
+        items: [
+            {
+                name: "TanStack Query",
+            },
+            {
+                name: "Shadcn",
+            },
+            {
+                name: "Redux",
+            },
+            {
+                name: "Zod",
+            },
+        ]
+    },
+    {
+        name: "Tools",
+        items: [
+            {
+                name: "Visual Studio Code",
+                level: "solid",
+            },
+            {
+                name: "Figma",
+                level: "basic",
+            },
+        ]
+    },
+    {
+        name: "Soft Skills",
+        items: [
+            {
+                name: "Team work",
             },
         ]
     },
